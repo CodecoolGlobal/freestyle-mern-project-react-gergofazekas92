425f0cbe8141d78d85e06266a296ef9cd52595d9
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-01-31 12:56:35 +0100

    css recipelist

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -36,17 +36,19 @@ function RecipeList({ onChangePage, onChooseRecipe }) {
     task()
   }, []);
 
-  function handleChoose(){
-
+  function handleChoose(uri){
+    console.log(uri);
+    onChooseRecipe(uri);
     onChangePage("recipe");
   }
 
   return (
     <>
-    <div>
-      <nav>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-        <button onClick={handleSearchClick}>🔎</button>
+    <div className="main">
+      <div>
+      <nav className="nav">
+        <input className="searchbar" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="search ingredients..."/>
+        <button className="searchbutton" onClick={handleSearchClick}>🔎</button>
       </nav>
     </div>
     
@@ -58,7 +60,7 @@ function RecipeList({ onChangePage, onChooseRecipe }) {
             <thead>
               <tr>
               <th >
-                <img src={`${recipe.recipe.images.SMALL.url}`} onClick={handleChoose}></img>
+                <img src={`${recipe.recipe.images.SMALL.url}`} onClick={() => handleChoose(recipe.recipe.uri)}></img>
               </th>
               </tr>
             </thead>
@@ -74,6 +76,8 @@ function RecipeList({ onChangePage, onChooseRecipe }) {
           </table>
         ))}
     </div>
+    </div>
+    
     </>
   )
 }
