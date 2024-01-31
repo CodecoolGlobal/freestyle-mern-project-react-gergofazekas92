7b5b5804b06e92c40534b8f9705b7673fae30f40
Gergo Fazekas <144519526+gergofazekas92@users.noreply.github.com>
2024-01-31 10:45:18 +0100

    display recipeslist

---
+++ client/src/components/RecipeList.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+
+function RecipeList({ onChangePage, onChooseRecipe }) {
+  const [input, setInput] = useState("");
+  const [recipes, setRecipes] = useState(null);
+  const [errorMes, setErrorMes] = useState("");
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=88394fd1&app_key=83cac76653be41a30be1b45298824ffd	`
+
+  async function fetchRecipes(url) {
+    try {
+      const response = await fetch(url);
+      const recipes = await response.json();
+      return recipes.hits;
+    }
+    catch (error) {
+      console.error("error fetching data:", error);
+    }
+  }
+
+  async function handleSearchClick(){
+    const fetchedRecipes = await fetchRecipes(url);
+    setRecipes(fetchedRecipes);
+
+    if (fetchedRecipes.length === 0) {
+      setErrorMes("No recipes found for the given search.");
+    } else {
+      setErrorMes("");
+    }
+  }
+
+  useEffect(() => {
+    async function task() { 
+      setRecipes(await fetchRecipes(url));
+    }
+    task()
+  }, []);
+
+  function handleChoose(){
+
+    onChangePage("recipe");
+  }
+
+  return (
+    <>
+    <div>
+      <nav>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button onClick={handleSearchClick}>🔎</button>
+      </nav>
+    </div>
+    
+    <div className="recipelist">
+    {errorMes && <p>{errorMes}</p>}
+      {recipes &&
+        recipes.map((recipe, index) => (
+          <table key={index}>
+            <thead>
+              <tr>
+              <th >
+                <img src={`${recipe.recipe.images.SMALL.url}`} onClick={handleChoose}></img>
+              </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>{`${recipe.recipe.label}`}</th>
+              </tr>
+              <tr>
+                <td>{`${Math.ceil(recipe.recipe.calories)} kcal`}</td>
+                <td>{`${recipe.recipe.mealType}`}</td>
+              </tr>
+            </tbody>
+          </table>
+        ))}
+    </div>
+    </>
+  )
+}
+
+export default RecipeList;
+
