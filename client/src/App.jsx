3a2a4a323757696c4bc7bec8b39b63ca99891e07
adesz0112 <meszaros.adam94@gmail.com>
2024-02-01 13:19:21 +0100

    Create favourite button and page to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,29 @@ import { useState } from 'react'
 import './App.css'
 import DisplayRecipe from './components/recipePage/DisplayRecipe'
 import RecipeList from './components/RecipeList'
-import ListComments from './components/ListComments';
 import "./DisplayRecipe.css"
+import Favourites from './components/Favourites';
 
 function App() {
   const [page, setPage] = useState("recipelist");
   const [recipeUrl, setRecipeUrl] = useState("")
 
+  function handleClick() {
+    setPage("favourites")
+  }
+
   return (
     <>
-    <header>
-      <div className='logo'>
-        <div className='foodlogo'>Food</div>
-        <div className='hublogo'>hub</div>
-      </div>
-    </header>
-    {page === "recipelist" && <RecipeList onChangePage={setPage} onChooseRecipe={setRecipeUrl}/> }
-    {page === "recipe" && <DisplayRecipe onData={recipeUrl} onChangePage={setPage}/> }
-    {/* {page === "shop" && <Shop /> } */}
+      <header>
+        <div className='logo'>
+          <div className='foodlogo'>Food</div>
+          <div className='hublogo'>hub</div>
+          <button onClick={handleClick}>❤️</button>
+        </div>
+      </header>
+      {page === "recipelist" && <RecipeList onChangePage={setPage} onChooseRecipe={setRecipeUrl} />}
+      {page === "recipe" && <DisplayRecipe onData={recipeUrl} onChangePage={setPage} />}
+      {page === "favourites" && <Favourites onChangePage={setPage} />}
     </>
   )
 }
