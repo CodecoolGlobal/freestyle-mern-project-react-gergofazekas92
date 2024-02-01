b0d2f5dcd2a645330c24e3f6bc0cb43273e17f32
melanihorvath <144622336+melanihorvath@users.noreply.github.com>
2024-02-01 11:57:03 +0100

    add css flexbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import DisplayRecipe from './components/recipePage/DisplayRecipe'
 import RecipeList from './components/RecipeList'
 import ListComments from './components/ListComments';
+import "./DisplayRecipe.css"
 
 function App() {
   const [page, setPage] = useState("recipelist");
